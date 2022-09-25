88022d3bb1b9d2d0b40e6f9653cf61b4895a4c14
akkm313 <91428773+akkm313@users.noreply.github.com>
2022-09-25 02:51:27 -0500

    changes for events

--- client/src/Campaigns.js
+++ client/src/Campaigns.js
@@ -1,9 +1,9 @@
 import React from 'react';
-import Object from './Object';
 import axios from 'axios'
 import {useEffect, useState} from 'react';
-import Font, { Text } from 'react-font'
 import Web3 from './Web3';
+import {FaSearch} from 'react-icons/fa';
+import Events from './Events';
 
 function Campaigns(){
 
@@ -11,12 +11,14 @@ function Campaigns(){
     const [query,setQuery] = useState('');
     const [toggleCampaign, setToggleCampaign] = useState(false);
 
+
     
 function populateEvents(){
     axios.get(`https://api.seatgeek.com/2/events?geoip=`+query+`&range=12mi&client_id=MjkzMzY2NTd8MTY2NDA2NDIwMy4zMTU4Nzc`)
     .then(response => {
-      setEvents(response.data.events)
-      console.log(events);
+
+    setEvents(response.data.events);
+ 
     })
 }    
 
@@ -37,11 +39,23 @@ return (
 
 
 
-
-
-<input onChange={e=>setQuery(e.target.value)}/> 
-<button onClick={populateEvents}>click </button>
-
+ {toggleCampaign && <div>
+ <input placeholder="Enter any zipcode" value={query} onChange={e=>setQuery(e.target.value)}/> 
+<button type="button" class="btn btn-primary" onClick={populateEvents}><FaSearch/> </button> 
+</div>
+ }
+{toggleCampaign && events.length>0 &&  events.map((e, i) => 
+    <div key={i} className="card" >
+    <div className="card-body">
+      <h5 className="card-title">{e.title+`-`+e.type}</h5>
+      <h6 className="card-subtitle mb-2 text-muted">{e.venue.name_v2+` `+e.venue.state}</h6>
+      <p class="card-text">{ e.datetime_local.substring(0,10)}</p>
+      <a target="_blank" href={e.url} className="card-link">Get More details</a>
+    </div>
+  </div>
+    
+    
+    )}
 
 
 </>
---
+++ client/src/Events.js
@@ -0,0 +1,14 @@
+import { events } from '@react-three/fiber';
+import React from 'react';
+
+function Events(props)
+{
+  const events = props.events;
+
+
+  events.length>0 && <p>h</p>
+
+
+
+}
+export default Events
